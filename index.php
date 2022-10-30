b5d919acc920288258fb101318c2659131d94732
stvnfrlls <stvnfrlls@gmail.com>
2022-10-30 22:33:15 +0800

    Test

--- index.php
+++ index.php
@@ -51,35 +51,7 @@
     </nav>
     <!-- Navbar End -->
 
-    <!-- Navbar Start -->
-    <nav class="navbar navbar-expand-lg bg-dark navbar-light sticky-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
-
-        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto p-4 p-lg-0 ">
-                <a href="index.html" class="nav-item nav-link active" style="color: white">Home</a>
-                <a href="about.html" class="nav-item nav-link" style="color: white">About Us</a>
-                <div class="nav-item dropdown">
-                    <a href="#" class="nav-item nav-link dropdown-toggle" data-bs-toggle="dropdown" style="color: white">Academics</a>
-                    <div class="dropdown-menu bg-dark border-0 m-0">
-                        <a href="feature.html" class="dropdown-item" style="color: white">Our Features</a>
-                        <a href="project.html" class="dropdown-item" style="color: white">Our Projects</a>
-                        <a href="team.html" class="dropdown-item" style="color: white">Team Members</a>
-                        <a href="appointment.html" class="dropdown-item" style="color: white">Appointment</a>
-                        <a href="testimonial.html" class="dropdown-item" style="color: white">Testimonial</a>
-                        <a href="404.html" class="dropdown-item" style="color: white">404 Page</a>
-                    </div>
-                </div>
-                <a href="service.html" class="nav-item nav-link" style="color: white">Admissions</a>
-                <a href="contact.html" class="nav-item nav-link" style="color: white">Scholarship and Discounts</a>
-                <a href="contact.html" class="nav-item nav-link" style="color: white">Contact Us</a>
-            </div>
-
-        </div>
-    </nav>
-    <!-- Navbar End -->
+    
     <!-- Carousel Start -->
     <div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="owl-carousel header-carousel position-relative">
