c4be84f8dca57ca6235c6a8aded71ef2e98fb2b4
stvnfrlls <stvnfrlls@gmail.com>
2022-11-09 00:16:32 +0800

    Changes with Navbar and Footer

--- faculty/dashboard.php
+++ faculty/dashboard.php
@@ -185,7 +185,7 @@
             </div>
         </div>
     </div>
-
+    
     <!-- Footer Start -->
     <div class="container-fluid bg-dark text-body footer mt-5 pt-5 px-0 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
@@ -231,20 +231,11 @@
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
                         &copy; <a href="#">Colegio De San Pedro</a>, All Right Reserved.
                     </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a href="https://htmlcodex.com">HTML Codex</a>
-                        <br> Distributed By: <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
     <!-- Footer End -->
-
-
-
-
 
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
