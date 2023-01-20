fa4eef0d4e863451bc02fafd4ec6f1bfe71ee5e9
stvnfrlls <stvnfrlls@gmail.com>
2023-01-20 13:10:26 +0800

    Updated Database Connection

--- assets/php/database.php
+++ assets/php/database.php
@@ -1,7 +1,8 @@
 <?php
+
 global $mysqli;
 
-$mysqli = new mysqli("localhost", "root", "", "sis_cdsp");
+$mysqli = new mysqli("localhost", "u952901270_admin0326", "giTG^W3y", "u952901270_sis_cdsp");
 
 if ($mysqli->connect_errno) {
     echo "Error Connecting: " . $mysqli->connect_error;
