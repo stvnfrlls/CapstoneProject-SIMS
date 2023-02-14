e4b65a90b4833bf8a82a1f79a64df83afa4084c6
stvnfrlls <stvnfrlls@gmail.com>
2023-02-15 04:00:46 +0800

    Added form submit

--- admin/createAnnouncement.php
+++ admin/createAnnouncement.php
@@ -171,66 +171,63 @@ if (!isset($_SESSION['AD_number'])) {
             <div class="main-panel">
                 <div class="content-wrapper">
                     <div class="row">
-                        <div class="col-sm-12">
-                            <div class="home-tab">
-                                <div class="d-sm-flex align-items-center justify-content-between border-bottom">
-                                    <div class="section-title text-center position-relative pb-3 mb-3 mx-auto">
-                                        <h2 class="fw-bold text-primary text-uppercase">Create Announcement</h2>
+                        <form action="" method="post">
+                            <div class="col-sm-12">
+                                <div class="home-tab">
+                                    <div class="d-sm-flex align-items-center justify-content-between border-bottom">
+                                        <div class="section-title text-center position-relative pb-3 mb-3 mx-auto">
+                                            <h2 class="fw-bold text-primary text-uppercase">Create Announcement</h2>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="tab-content tab-content-basic">
-                                    <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview">
-                                        <div class="row">
-                                            <div class="col-12 grid-margin">
-                                                <div class="card">
-                                                    <div class="card-body">
-                                                        <h4 class="card-title">Create Reminders</h4>
-                                                        <form class="form-sample" action="confirmfaculty.php" method="POST">
-
+                                    <div class="tab-content tab-content-basic">
+                                        <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview">
+                                            <div class="row">
+                                                <div class="col-12 grid-margin">
+                                                    <div class="card">
+                                                        <div class="card-body">
+                                                            <h4 class="card-title">Create Reminders</h4>
                                                             <div class="row">
                                                                 <div class="row g-3">
                                                                     <div class="col-md-6">
                                                                         <div class="form-floating">
-                                                                            <input type="text" class="form-control" id="name" value="<?php echo "ADMIN: " . $adminData['AD_name'] ?>" placeholder="Your Name" readonly>
+                                                                            <input type="text" class="form-control" id="name" name="author" value="<?php echo "ADMIN: " . $adminData['AD_name'] ?>" placeholder="Your Name" readonly>
                                                                             <label for="name">Your Name</label>
                                                                         </div>
                                                                     </div>
                                                                     <div class="col-md-6">
                                                                         <div class="form-floating">
-                                                                            <input type="email" class="form-control" value="<?php echo  date('M/d/Y') ?>" readonly>
+                                                                            <input type="email" class="form-control" name="date" value="<?php echo  date('M/d/Y') ?>" readonly>
                                                                             <label for="email">Date of the Event</label>
                                                                         </div>
                                                                     </div>
 
                                                                     <div class="col-12">
                                                                         <div class="form-floating">
-                                                                            <input type="text" class="form-control" id="subject" placeholder="Subject">
+                                                                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject">
                                                                             <label for="subject">Title</label>
                                                                         </div>
                                                                     </div>
                                                                     <div class="col-12">
                                                                         <div class="form-floating">
-                                                                            <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 250px"></textarea>
+                                                                            <textarea class="form-control" placeholder="Leave a message here" id="message" name="message" style="height: 250px"></textarea>
                                                                             <label for="message">Description</label>
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
-
-                                                        </form>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
-
                                         </div>
                                     </div>
-                                </div>
 
+                                </div>
                             </div>
-                        </div>
-                        <form style="text-align: center;">
-                            <button type="submit" class="btn btn-primary me-2">Post</button>
-                            <button class="btn btn-light">Back</button>
+                            <div style="text-align: center;">
+                                <button type="submit" name="postAnnouncement" class="btn btn-primary me-2">Post</button>
+                                <button class="btn btn-light">Back</button>
+                            </div>
                         </form>
                     </div>
                 </div>
