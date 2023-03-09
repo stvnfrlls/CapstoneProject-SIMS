c9d03df8e19c41ce8c0b3cd01515b95abdeda2ae
siyemel <sabile.camillepup@gmail.com>
2023-03-10 03:13:30 +0800

    removed the back to top buttons fixed the dead links

--- student/reminders.php
+++ student/reminders.php
@@ -75,7 +75,7 @@ if (!isset($_SESSION['SR_number'])) {
             </style>
             <div class="navbar-nav m-auto p-4 p-lg-0 ">
                 <a href="../index.php" class="nav-item nav-link active" style="color: white; font-size: 14px;">Home</a>
-                <a href="" class="nav-item nav-link" style="color: white; font-size: 14px;">About Us</a>
+                <a href="../about.php" class="nav-item nav-link" style="color: white; font-size: 14px;">About Us</a>
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-item nav-link" data-bs-toggle="dropdown" style="color: white; font-size: 14px;">Menu <i class="fa fa-caret-down"></i></a>
                     <div class="dropdown-menu bg-dark border-0 m-0">
@@ -88,7 +88,7 @@ if (!isset($_SESSION['SR_number'])) {
                         <a href="../auth/logout.php" class="dropdown-item" style="color: white; font-size: 14px; text-align:left;">Logout</a>
                     </div>
                 </div>
-                <a href="" class="nav-item nav-link" style="color: white; font-size: 14px;">Faculty Directory</a>
+                <a href="../faculty.php" class="nav-item nav-link" style="color: white; font-size: 14px;">Faculty Directory</a>
             </div>
         </div>
     </nav>
