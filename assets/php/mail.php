71fbd39c96d7d631a6364d8eed46fe1e39dafbf7
Steven Frilles <92411841+stvnfrlls@users.noreply.github.com>
2023-03-09 16:11:44 +0800

    Updated path

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -2,9 +2,9 @@
 
 use PHPMailer\PHPMailer\PHPMailer;
 
-require_once __DIR__ . '..\..\PHPMailer\src\Exception.php';
-require_once __DIR__ . '..\..\PHPMailer\src\PHPMailer.php';
-require_once __DIR__ . '..\..\PHPMailer\src\SMTP.php';
+require_once '..\PHPMailer\src\Exception.php';
+require_once '..\PHPMailer\src\PHPMailer.php';
+require_once  '..\PHPMailer\src\SMTP.php';
 
 $mail = new PHPMailer();
 $mail->isSMTP();
