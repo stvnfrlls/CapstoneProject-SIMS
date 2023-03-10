906f1a851613bbaf1861ff03f647afacf2fccf95
Steven Frilles <92411841+stvnfrlls@users.noreply.github.com>
2023-03-10 23:29:58 +0800

    changed database credentials

--- assets/php/database.php
+++ assets/php/database.php
@@ -2,7 +2,7 @@
 
 global $mysqli;
 
-$mysqli = new mysqli("localhost", "u952901270_admin0326", "giTG^W3y", "u952901270_sis_cdsp");
+$mysqli = new mysqli("localhost", "root", "", "sis_cdsp");
 
 if ($mysqli->connect_errno) {
     echo "Error Connecting: " . $mysqli->connect_error;
