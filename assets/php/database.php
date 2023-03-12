7e1a98f2fcd86966d0c44a7d43da24b524e1d816
Steven Frilles <92411841+stvnfrlls@users.noreply.github.com>
2023-03-12 13:55:57 +0800

    Update database.php

--- assets/php/database.php
+++ assets/php/database.php
@@ -2,7 +2,7 @@
 
 global $mysqli;
 
-$mysqli = new mysqli("localhost", "u952901270_admin2311	", "giTG^W3y", "u952901270_sforms_cdsp");
+$mysqli = new mysqli("localhost", "u952901270_admin2311	", "Eleven.11", "u952901270_sforms_cdsp");
 
 if ($mysqli->connect_errno) {
     echo "Error Connecting: " . $mysqli->connect_error;
