fb4207a5976d2077fca9a82002dac6be52b63294
siyemel <sabile.camillepup@gmail.com>
2023-03-15 22:44:55 +0800

    changed ui for changeing password

---
+++ faculty/changePassword.php
@@ -0,0 +1,263 @@
+<?php
+require_once("../assets/php/server.php");
+
+if (!isset($_SESSION['F_number'])) {
+    header('Location: ../auth/login.php');
+} else {
+    $facultyInformation = $mysqli->query("SELECT * FROM faculty WHERE F_number = '{$_SESSION['F_number']}'");
+    $faculty = $facultyInformation->fetch_assoc();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Change Password</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="../assets/img/favicon.png" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css" rel="stylesheet">
+    <link href="../assets/css/sweetAlert.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="../assets/lib/animate/animate.min.css" rel="stylesheet">
+    <link href="../assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="../assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="../assets/css/style.css" rel="stylesheet">
+    <link href="../assets/css/form-style.css" rel="stylesheet">
+    <link href="../assets/css/admin/style.css" rel="stylesheet">
+    <link href="../assets/css/admin/materialdesignicons.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- Navbar Start -->
+    <nav class="fixed-top align-items-top">
+        <nav class="navbar navbar-expand-lg bg-primary navbar-light py-lg-0 px-lg-5">
+            <img class="m-3" href="../index.php" src="../assets/img/logo.png" style="height: 50px; width:300px;" alt="Icon">
+            <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-bs-toggle="offcanvas">
+                <span class="fa fa-bars"></span>
+            </button>
+        </nav>
+    </nav>
+    <!-- Navbar End -->
+
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper">
+            <nav class="sidebar sidebar-offcanvas" id="sidebar">
+                <ul class="nav">
+                    <li class="nav-item" style="text-align:center; font-size: 20px; color: #b9b9b9; margin-top:20px;">FACULTY</li>
+                    <!-- line 1 -->
+                    <li class="nav-item nav-category">Profile</li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../faculty/dashboard.php">
+                            <i class=""></i>
+                            <span class="menu-title">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../faculty/viewProfile.php">
+                            <i class=""></i>
+                            <span class="menu-title">View Profile</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../faculty/reminders.php">
+                            <i class=""></i>
+                            <span class="menu-title">Reminders</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../faculty/announcement.php">
+                            <i class=""></i>
+                            <span class="menu-title">School Announcements</span>
+                        </a>
+                    </li>
+                    <!-- line 2 -->
+                    <li class="nav-item nav-category">Menu</li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../faculty/scanQR.php">
+                            <i class=""></i>
+                            <span class="menu-title">Scan QR</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../faculty/advisoryPage.php">
+                            <i class=""></i>
+                            <span class="menu-title">Advisory Class</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../faculty/classList.php">
+                            <i class=""></i>
+                            <span class="menu-title">Class List</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../faculty/encodegrades.php">
+                            <i class=""></i>
+                            <span class="menu-title">Encode Grades</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../faculty/studentStatus.php">
+                            <i class=""></i>
+                            <span class="menu-title">Student Status</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../faculty/dailyReports.php">
+                            <i class=""></i>
+                            <span class="menu-title">Attendance Report</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../auth/logout.php">
+                            <i class=""></i>
+                            <span class="menu-title">Logout</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    <form action="<?php $_SERVER["PHP_SELF"] ?>" enctype="multipart/form-data" method="post" id="editfacultyProfile">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="home-tab">
+                                    <div class="d-sm-flex align-items-center justify-content-between">
+                                        <div class="section-title text-center position-relative pb-3 mb-3 mx-auto">
+                                            <h2 class="fw-bold text-primary text-uppercase">Profile Information</h2>
+                                        </div>
+                                    </div>
+                                    <div class="container-xl px-4 mt-4" style="padding-bottom:0px">
+                                        <nav class="nav">
+                                            <a class="nav-link active ms-0" href="../student/editProfile.php" style="color: #c02628;">Edit Profile</a>
+                                            <a class="nav-link" href="../student/changePassword.php">Change Password</a>
+                                        </nav>
+                                        <div class="border-bottom"></div>
+                                    </div>
+                                    <div style="text-align: right; margin-top: 15px">
+                                        <input type="hidden" name="updateProfile" value="submit">
+                                        <button id="updateProfile" name="updateProfile" class="btn btn-primary me-2">Save</button>
+                                        <button class="btn btn-light" onclick="location.href='../faculty/viewProfile.php'">Back</button>
+                                    </div>
+                                    <div class="tab-content tab-content-basic">
+                                        <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview">
+
+                                            <div class="row">
+                                                <div class="col-lg-6 col-sm-12 grid-margin" style="margin:auto">
+                                                    <div class="card">
+                                                        <div class="card-body">
+                                                            <h4 class="card-title">Change Password</h4>
+                                                            <div class="row">
+                                                                <div class="col-12">
+                                                                    <label label class="col-sm-12 col-form-label">Enter Current Password</label>
+                                                                    <div class="col-sm-12">
+                                                                        <input type="password" class="form-control" id="newPassword" name="newPassword" required disabled>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col-12">
+                                                                    <label label class="col-sm-12 col-form-label">Enter New Password</label>
+                                                                    <div class="col-sm-12">
+                                                                        <input type="password" class="form-control" id="newPassword" name="newPassword" required disabled>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="row">
+                                                                <div class="col-12">
+                                                                    <label label class="col-sm-12 col-form-label">Confirm New Password</label>
+                                                                    <div class="col-sm-12">
+                                                                        <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required disabled>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <!-- content-wrapper ends -->
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-body footer wow fadeIn" data-wow-delay="0.1s">
+        <div class="container-fluid copyright" style="padding: 15px 0px 15px 0px;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a href="#">Colegio De San Pedro</a>, All Right Reserved.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+
+    <!-- JavaScript Libraries -->
+
+
+    <!-- Template Javascript -->
+    <script src="../assets/js/main.js"></script>
+
+    <script src="../assets/js/admin/vendor.bundle.base.js"></script>
+    <script src="../assets/js/admin/off-canvas.js"></script>
+    <script src="../assets/js/admin/file-upload.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.4/dist/sweetalert2.min.js"></script>
+    <script>
+        const currentEmail = document.getElementById('currentEmail');
+        const F_email = document.getElementById('F_email');
+        const newPassword = document.getElementById('newPassword');
+        const confirmPassword = document.getElementById('confirmPassword');
+        document.getElementById('enableUpdateForm').addEventListener('click', function() {
+            if (F_email.disabled && newPassword.disabled && confirmPassword.disabled) {
+                F_email.removeAttribute("disabled");
+                newPassword.removeAttribute("disabled");
+                confirmPassword.removeAttribute("disabled");
+            } else {
+                F_email.disabled = true;
+                newPassword.disabled = true;
+                confirmPassword.disabled = true;
+            }
+        })
+    </script>
+</body>
+
+</html>
