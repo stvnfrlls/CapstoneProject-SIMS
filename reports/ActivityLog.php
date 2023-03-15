9a00c1312146da576f93bc4a5c963f34b358cc32
Steven Frilles <92411841+stvnfrlls@users.noreply.github.com>
2023-03-15 15:35:48 +0800

    Changes with database credentials

--- reports/ActivityLog.php
+++ reports/ActivityLog.php
@@ -10,7 +10,7 @@ if (isset($_GET['start_date']) && isset($_GET['end_date'])) {
 
         function Header()
         {
-            $mysqli = new mysqli("localhost", "u952901270_admin0326", "giTG^W3y", "u952901270_sis_cdsp");
+            $mysqli = new mysqli("localhost", "root", "", "sis_cdsp");
             $getAcadYear = $mysqli->query("SELECT * FROM acad_year");
             $acadYear_Data = $getAcadYear->fetch_assoc();
             //Logo Image
