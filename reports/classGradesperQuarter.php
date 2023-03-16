fed7a2a67bcdf8cd57710473986340407d7ed29a
Steven Frilles <92411841+stvnfrlls@users.noreply.github.com>
2023-03-16 20:59:18 +0800

    Update classGradesperQuarter.php

--- reports/classGradesperQuarter.php
+++ reports/classGradesperQuarter.php
@@ -7,7 +7,7 @@ class PDF extends FPDF
 {
     function Header()
     {
-        $mysqli = new mysqli("localhost", "root", "", "sis_cdsp");
+        $mysqli = new mysqli("localhost", "u952901270_admin2311", "Eleven.11", "u952901270_sforms_cdsp");
         $getAcadYear = $mysqli->query("SELECT * FROM acad_year");
         $acadYear_Data = $getAcadYear->fetch_assoc();
         //Logo Image
